9dbebcb936e513bc7a1103b6b251292c1fd2f488
Pavel Veremenyuk <pveremenyuk@iba.by>
2017-01-30 17:19:21 +0300

    Задание 3. Сценарий логина litecard/admin

---
+++ java-example/src/test/java/by/iba/training/selenium/LitecardAdminLogon.java
@@ -0,0 +1,43 @@
+package by.iba.training.selenium;
+
+/**
+ * Created by VEREMENYUK_P on 1/30/2017.
+ */
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class LitecardAdminLogon {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void LitecardAdminLogon () {
+        driver.get("http://localhost/litecard/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
